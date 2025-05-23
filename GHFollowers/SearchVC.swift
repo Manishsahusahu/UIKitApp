438c73b610fe9e3d5f6698d11d1678354cb9d191
Manish Sahu <sahumanish327@gmail.com>
2025-05-23 17:35:32 +0530

    button textfield and logo image setup in search screen

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -8,11 +8,59 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let userNameTextField = GFTextField()
+    let calltoActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        view.backgroundColor = .systemPink
+        
         self.edgesForExtendedLayout = []
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTextField()
+        configureCTAButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    private func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "logo")
+        
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    private func configureTextField() {
+        view.addSubview(userNameTextField)
+        
+        NSLayoutConstraint.activate([
+            userNameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            userNameTextField.heightAnchor.constraint(equalToConstant: 50),
+            userNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            userNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+        ])
+    }
+    
+    private func configureCTAButton() {
+        view.addSubview(calltoActionButton)
+        
+        NSLayoutConstraint.activate([
+            calltoActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -48),
+            calltoActionButton.heightAnchor.constraint(equalToConstant: 50),
+            calltoActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            calltoActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+        ])
     }
 }
