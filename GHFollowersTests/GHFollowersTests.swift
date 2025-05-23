969c6e3cfa9e89515b80e243db161907b4cfef68
Manish Sahu <sahumanish327@gmail.com>
2025-05-23 13:53:43 +0530

    Initial Commit

---
+++ GHFollowersTests/GHFollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersTests.swift
+//  GHFollowersTests
+//
+//  Created by Manish sahu on 23/05/25.
+//
+
+import Testing
+@testable import GHFollowers
+
+struct GHFollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
