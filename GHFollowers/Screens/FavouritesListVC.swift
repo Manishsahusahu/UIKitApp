bc35bd18b7e12597e1516844600bf01f09dd9154
Manish Sahu <sahumanish327@gmail.com>
2025-06-02 17:12:49 +0530

    configuring the table view

--- GHFollowers/Screens/FavouritesListVC.swift
+++ GHFollowers/Screens/FavouritesListVC.swift
@@ -8,11 +8,55 @@
 import UIKit
 
 class FavouritesListVC: UIViewController {
+    let tableView = UITableView()
+    var favorites: [Follower] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
+        configureViewcontroller()
+    }
+    
+    private func configureViewcontroller() {
+        view.backgroundColor = .systemBackground
         self.edgesForExtendedLayout = []
+        title = "Favorites"
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseId)
+    }
+    
+    private func getFavoritesData() {
+        PersistenceManager.retreiveFavorites { [weak self] result in
+            guard let self else { return }
+            
+            switch result {
+            case .success(let favorites):
+                self.favorites = favorites
+            case .failure(let failure):
+                break
+            }
+        }
+    }
+}
+
+extension FavouritesListVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseId) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+        
+        cell.set(favorite: favorite)
+        return cell
     }
 }
